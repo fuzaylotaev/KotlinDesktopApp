98e99d0fc3d3d7d69b2abce960168e9a7e5fe2a0
fuzayl <otaev.fuzayl@gmail.com>
2026-05-15 16:15:57 +0200

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/org/example/project/api/ApiService.kt
@@ -0,0 +1,85 @@
+package org.example.project.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.URLProtocol
+import io.ktor.http.isSuccess
+import io.ktor.http.path
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonObject
+import org.example.project.model.User
+
+class ApiService {
+
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 15000
+        }
+    }
+
+    suspend fun fetchData(): String {
+        var result = httpClient.get {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "dummyjson.com"
+                path("test")
+            }
+        }
+
+        return if (result.status.isSuccess()) {
+            result.bodyAsText()
+        } else result.status.description
+    }
+
+    suspend fun searchUsers(q: String): List<User> {
+        val response = httpClient.get {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "dummyjson.com"
+                path("users/search")
+                parameter("q", q)
+            }
+        }
+        if (response.status.isSuccess()) {
+            val responseJson = Json.parseToJsonElement(response.bodyAsText())
+            val usersJson = responseJson.jsonObject["users"] ?: throw IllegalArgumentException("Invalid 'users' key")
+            return Json.decodeFromJsonElement<List<User>>(usersJson)
+        } else {
+            return emptyList()
+        }
+    }
+
+    suspend fun getUsers(limit: Int = 10, skip: Int = 0): List<User> {
+        val response = httpClient.get {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "dummyjson.com"
+                path("users/search")
+                parameter("limit", limit)
+                parameter("skip", skip)
+            }
+        }
+
+        if (response.status.isSuccess()) {
+            val responseJson = Json.parseToJsonElement(response.bodyAsText())
+            val usersJson = responseJson.jsonObject["users"] ?: throw IllegalArgumentException("Invalid 'users' key")
+            return Json.decodeFromJsonElement<List<User>>(usersJson)
+        } else {
+            return emptyList()
+        }
+    }
+}
---
+++ composeApp/src/jvmMain/kotlin/org/example/project/model/User.kt
@@ -0,0 +1,14 @@
+package org.example.project.model
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
+
+@Serializable
+@JsonIgnoreUnknownKeys
+data class User(
+    val id: Long,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String
+)
